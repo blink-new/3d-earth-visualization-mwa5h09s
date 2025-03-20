065d3c920a50f72180ae832c1cb883f2c3de812a
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-03-20 23:11:20 +0000

    Fix Vite Config TypeScript

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import type { UserConfig } from 'vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    cors: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      protocol: 'wss',
+      clientPort: 443,
+      timeout: 120000
+    },
+    allowedHosts: ['.blink.new']
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  }
+} satisfies UserConfig)
